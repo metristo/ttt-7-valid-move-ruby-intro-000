a30b3cb9e9de1fa50579d35943326c8d46aa0868
Michael G Rivera <rivera.mgr@gmail.com>
2020-05-19 03:17:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,9 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if position_taken
+  if position_taken?
     false
   elsif index.between?(0, 8)
-    true 
+    true
   end
 end
 
